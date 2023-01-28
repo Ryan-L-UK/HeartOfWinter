26f2c6fd0fe02ecac59a6643c435a4e8c3b90be9
Ryan-L-UK <Ryanlewery@Gmail.com>
2023-01-28 15:41:55 +0000

    New Adventure Menu
    A new and improved menu system for the adventures to be loaded. Along with supporting JS files.

--- Library/JavaScript/AdventureScript.js
+++ Library/JavaScript/AdventureScript.js
@@ -1,50 +1,61 @@
 //-----------------------------------------
+//Adventure Placeholder
+//-----------------------------------------
+fetch("http://localhost:8080/Sources/Adventures/Adv 0.0 Placeholder.html")
+  .then(function (Aresponse) {
+    // When the page is loaded convert it to text
+    return Aresponse.text();
+  })
+  .then(function (AdvHTML) {
+    // Initialize the DOM parser
+    var parser = new DOMParser();
+
+    // Parse the text
+    var AdvDoc = parser.parseFromString(AdvHTML, "text/html");
+
+    // You can now even select part of that html as you would in the regular DOM
+    // Example:
+    var AdvArticle = AdvDoc.querySelector("html").innerHTML;
+    document.getElementById("Adv_output").innerHTML = AdvArticle;
+    console.log(AdvDoc);
+    console.log("Adventure Splash Loaded Successfully...");
+  })
+  .catch(function (err) {
+    console.log("ERROR: Failed to load splash: ", err);
+  });
+
+//-----------------------------------------
 //Adventure Selector
 //-----------------------------------------
-function showInput() {
-  function checkadv(adventure) {
-    var advresult = "";
-    var lookup = {
-      "": "Error",
-      "Adv 1.1 A Gullet Cove Hello":
-        "http://localhost:8080/Sources/Adventures/Adv 1.1 A Gullet Cove Hello.html",
-      "Adv 2.1 The Tainted Catnip":
-        "http://localhost:8080/Sources/Adventures/Adv 2.1 The Tainted Catnip.html",
-      "Adv 2.2 Quest For Atonement":
-        "http://localhost:8080/Sources/Adventures/Adv 2.2 Quest For Atonement.html",
-      "Adv 3.1 The Sleep Of The Dreamless":
-        "http://localhost:8080/Sources/Adventures/Adv 3.1 The Sleep Of The Dreamless.html",
-      "Adv 3.2 Solstice Shenanigans":
-        "http://localhost:8080/Sources/Adventures/Adv 3.2 Solstice Shenanigans.html",
-      "Adv 4.1 The Brink Of War":
-        "http://localhost:8080/Sources/Adventures/Adv 4.1 The Brink Of War.html",
-      "Adv 4.2": "http://localhost:8080/Sources/Adventures/TBC",
-    };
-    advresult = lookup[adventure];
-    console.log(advresult);
-    AdvOut = advresult;
-  }
-  checkadv(document.getElementById("Adv_input").value);
-  fetch(AdvOut)
-    .then(function (Lresponse) {
-      // When the page is loaded convert it to text
-      return Lresponse.text();
-    })
-    .then(function (AdventureHTML) {
-      // Initialize the DOM parser
-      var parser = new DOMParser();
 
-      // Parse the text
-      var AdventureDoc = parser.parseFromString(AdventureHTML, "text/html");
+document.querySelectorAll(".adv").forEach((element) => {
+  //add an event listener to each one
+  element.addEventListener("click", (event) => {
+    //prevent the default behaviour of the event (in this instance it would reload the page with url parameters set)
+    event.preventDefault();
+    //set the attribute src on the frame element to the target href (target being the a tag)
+    AdvOut = event.target.getAttribute("href");
+    fetch(AdvOut)
+      .then(function (Lresponse) {
+        // When the page is loaded convert it to text
+        return Lresponse.text();
+      })
+      .then(function (AdventureHTML) {
+        // Initialize the DOM parser
+        var parser = new DOMParser();
 
-      // You can now even select part of that html as you would in the regular DOM
-      // Example:
-      var AdventureArticle = AdventureDoc.querySelector("html").innerHTML;
-      document.getElementById("Adv_output").innerHTML = AdventureArticle;
-      console.log(AdventureDoc);
-      console.log("Adventure Loaded Successfully...");
-    })
-    .catch(function (err) {
-      console.log("ERROR: Adventure failed to load: ", err);
-    });
-}
+        // Parse the text
+        var AdventureDoc = parser.parseFromString(AdventureHTML, "text/html");
+
+        // You can now even select part of that html as you would in the regular DOM
+        // Example:
+        var AdventureArticle = AdventureDoc.querySelector("html").innerHTML;
+        document.getElementById("Adv_output").innerHTML = AdventureArticle;
+        console.log(AdventureDoc);
+        console.log("Adventure Loaded Successfully...");
+      })
+      .catch(function (err) {
+        console.log("ERROR: Adventure failed to load: ", err);
+      });
+  });
+});
