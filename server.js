2cb91441ffb753d5918cb85c182ead052d726990
benmiles98@gmail.com <benmiles98@gmail.com>
2023-03-17 18:25:57 +0000

    remove comment

--- server.js
+++ server.js
@@ -35,21 +35,3 @@ function readDirectory(folder, res){
     res.send(data);
   })
 }
-/*http
-  .createServer(function (req, res) {
-    var parsedUrl = new URL(req.url, `http://${req.headers.host}`);
-    fs.readFile(
-      __dirname + decodeURI(parsedUrl.pathname),
-      function (err, data) {
-        if (err) {
-          res.writeHead(404);
-          res.end(JSON.stringify(err));
-          return;
-        }
-        res.writeHead(200);
-        res.end(data);
-      }
-    );
-  })
-  .listen(8080);
-console.log("Server started successfully, do not close this terminal");*/
